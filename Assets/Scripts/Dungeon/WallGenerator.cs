6d7ba37f732bb8d6999790f9c2fadcdfec9306ec
Note16 <vincent.van.lohuizen@realproduction.nl>
2023-04-30 15:44:21 +0200

    Dungeon Generator (#4)
    * Added dungeon generator
    
    * rename editor class and cleanup direction2d

---
+++ Assets/Scripts/Dungeon/WallGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WallGenerator
+{
+    public static void CreateWalls(HashSet<Vector2Int> floorPositions, TilemapVisualizer tilemapVisualizer)
+    {
+        var basicWallPositions = FindWallsInDirections(floorPositions, Direction2D.cardinalDirectionsList);
+        CreateBasicWall(tilemapVisualizer, basicWallPositions);
+    }
+
+    private static void CreateBasicWall(TilemapVisualizer tilemapVisualizer, HashSet<Vector2Int> basicWallPositions)
+    {
+        foreach (var position in basicWallPositions)
+        {
+            tilemapVisualizer.PaintSingleBasicWall(position);
+        }
+    }
+
+    private static HashSet<Vector2Int> FindWallsInDirections(HashSet<Vector2Int> floorPositions, List<Vector2Int> directionList)
+    {
+        HashSet<Vector2Int> wallPositions = new();
+        foreach (var position in floorPositions)
+        {
+            foreach (var direction in directionList)
+            {
+                var neighbourPosition = position + direction;
+                if (floorPositions.Contains(neighbourPosition) == false)
+                    wallPositions.Add(neighbourPosition);
+            }
+        }
+        return wallPositions;
+    }
+}
