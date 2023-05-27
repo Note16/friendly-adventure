2feaec6d968cfee576fb7329e012a4db7414fa56
Note <vincent.van.lohuizen@realproduction.nl>
2023-05-28 00:53:54 +0200

    updated boss enemy

--- Assets/Scripts/Characters/Enemies/Enemy.cs
+++ Assets/Scripts/Characters/Enemies/Enemy.cs
@@ -16,7 +16,7 @@ namespace Assets.Scripts.Characters.Enemies
         private float meleeAttackRange = 4f;
 
         [SerializeField]
-        private bool allowSpriteFlip = true;
+        private bool spriteFacingRight = true;
 
         private PlayerController playerController;
         private SpriteRenderer spriteRenderer;
@@ -75,10 +75,16 @@ namespace Assets.Scripts.Characters.Enemies
             var moveSuccess = movement.Move(moveInput);
             animator.SetBool("isMoving", moveSuccess);
 
-            if (allowSpriteFlip)
+
+            if (spriteFacingRight)
             {
                 // If we are moving left flip sprite!
                 spriteRenderer.flipX = moveInput.x < 0;
+            }
+            else
+            {
+                // If we are moving right flip sprite!
+                spriteRenderer.flipX = moveInput.x > 0;
             }
         }
 
