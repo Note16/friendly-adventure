17ab92c439ab124491f19ef61bb3b6ec75598585
Note <vincent.van.lohuizen@realproduction.nl>
2023-05-27 22:31:26 +0200

    moved objects generation to objectGenerator.cs

---
+++ Assets/Scripts/Dungeon/ObjectsGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts.Dungeon.Areas.Rooms;
+using Assets.Scripts.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Dungeon
+{
+    [Serializable]
+    public class NamedGameObject
+    {
+        public string Name;
+        public List<GameObject> GameObjects;
+    }
+
+    public class ObjectsGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private List<NamedGameObject> dungeonObjects;
+
+        private List<GameObject> activeObjects;
+
+        public ObjectsGenerator()
+        {
+            activeObjects = new List<GameObject>();
+        }
+
+        public void GenerateExit(Room room)
+        {
+            var exitObj = dungeonObjects.FirstOrDefault(obj => obj.Name == "Exits")?.GameObjects.FirstOrDefault();
+            var position = new Vector2Int(room.Rect.xMax - 4, room.WallTopRect.yMin + 3);
+            activeObjects.Add(GameObject.Instantiate(exitObj, (Vector3Int)position, Quaternion.identity, transform));
+        }
+
+        public void DestroyObjects()
+        {
+            if (activeObjects.Any())
+                GameObjectHelper.Destroy(activeObjects);
+        }
+    }
+}
---
+++ Assets/Scripts/Helpers/GameObjectHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+    public static class GameObjectHelper
+    {
+        public static void Destroy<T>(IEnumerable<T> objects) where T : Object
+        {
+            if (objects == null)
+                return;
+
+            foreach (var gameObject in objects)
+            {
+                if (gameObject == null)
+                    continue;
+
+                if (Application.IsPlaying(gameObject))
+                    Object.Destroy(gameObject);
+                else
+                    Object.DestroyImmediate(gameObject);
+            }
+        }
+
+        public static void DestroyChildren(Transform transform)
+        {
+            for (int i = transform.childCount; i > 0; --i)
+            {
+                if (Application.IsPlaying(transform.GetChild(0).gameObject))
+                    Object.Destroy(transform.GetChild(0).gameObject);
+                else
+                    Object.DestroyImmediate(transform.GetChild(0).gameObject);
+            }
+        }
+    }
+}
