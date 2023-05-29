31b703e47ee955095bbe9bcd76ff5e6777a23363
Note16 <vincent.van.lohuizen@realproduction.nl>
2023-05-29 20:51:16 +0200

    added combat text (#20)

---
+++ Assets/Scripts/Helpers/ResourceHelper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+    public static class ResourceHelper
+    {
+        public static GameObject CombatText => (GameObject)Resources.Load("CombatText/TextPopup", typeof(GameObject));
+    }
+}
