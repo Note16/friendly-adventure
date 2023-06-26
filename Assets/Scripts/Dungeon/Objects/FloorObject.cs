7dd91e779e73305967305ac2ce7ef5ce37b3a7b2
Note <vincent.van.lohuizen@realproduction.nl>
2023-06-26 22:04:47 +0200

    added dungeon objects

---
+++ Assets/Scripts/Dungeon/Objects/FloorObject.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Characters.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.Dungeon.Objects
+{
+    public class FloorObject : MonoBehaviour
+    {
+        protected PlayerController playerController;
+        protected SpriteRenderer spriteRenderer;
+
+        private void OnEnable()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            playerController = FindObjectOfType<PlayerController>();
+        }
+
+        private void FixedUpdate()
+        {
+            // Update sorting order so enemy shows on top of player
+            // When player is positioned above the enemy
+            var relativePos = (Vector2)playerController.transform.position - (Vector2)transform.position;
+
+            var pivitPoint = spriteRenderer.size.y - 3f;
+            spriteRenderer.sortingOrder = relativePos.y < pivitPoint ? 0 : 2;
+        }
+
+    }
+}
--- Assets/Scripts/Dungeon/ObjectsGenerator.cs
+++ Assets/Scripts/Dungeon/ObjectsGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Dungeon.Areas.Rooms;
+using Assets.Scripts.Extensions;
 using Assets.Scripts.Helpers;
 using System;
 using System.Collections.Generic;
@@ -19,24 +20,61 @@ namespace Assets.Scripts.Dungeon
         [SerializeField]
         private List<NamedGameObject> dungeonObjects;
 
-        private List<GameObject> activeObjects;
+        private HashSet<GameObject> activeObjects;
 
         public ObjectsGenerator()
         {
-            activeObjects = new List<GameObject>();
+            activeObjects = new HashSet<GameObject>();
+        }
+
+        public void GenerateWallObjects(Room room)
+        {
+            var wallItems = dungeonObjects.FirstOrDefault(obj => obj.Name == "Wall");
+            if (wallItems != null)
+            {
+                var wallItem = RandomHelper.GetRandom(wallItems.GameObjects);
+                var leftPosition = new Vector2Int(room.Rect.xMin + 4, room.Walls.Top.yMin + 3);
+                activeObjects.Add(Instantiate(wallItem, (Vector3Int)leftPosition, Quaternion.identity, transform));
+
+                var rightPosition = new Vector2Int(room.Rect.xMax - 4, room.Walls.Top.yMin + 3);
+                activeObjects.Add(Instantiate(wallItem, (Vector3Int)rightPosition, Quaternion.identity, transform));
+            }
+        }
+
+        public void GenerateFloorObjects(Room room)
+        {
+            var floorItems = dungeonObjects.FirstOrDefault(obj => obj.Name == "Floor");
+            if (floorItems != null)
+            {
+                var roomFloor = room.Floor.Inner.allPositionsWithin.ToVector2Int();
+
+                var itemCount = 8;
+
+                for (int i = 0; i < itemCount; i++)
+                {
+                    var randomPosition = (Vector3Int)RandomHelper.GetRandom(roomFloor);
+
+                    // Do not generate object if on already exists in < 1f distance
+                    if (!activeObjects.Any(obj => Vector3.Distance(obj.transform.position, randomPosition) < 1f))
+                        activeObjects.Add(Instantiate(RandomHelper.GetRandom(floorItems.GameObjects), randomPosition, Quaternion.identity, transform));
+                }
+            }
         }
 
         public void GenerateExit(Room room)
         {
             var exitObj = dungeonObjects.FirstOrDefault(obj => obj.Name == "Exits")?.GameObjects.FirstOrDefault();
             var position = new Vector2Int(room.Rect.xMax - 4, room.Walls.Top.yMin + 3);
-            activeObjects.Add(GameObject.Instantiate(exitObj, (Vector3Int)position, Quaternion.identity, transform));
+            activeObjects.Add(Instantiate(exitObj, (Vector3Int)position, Quaternion.identity, transform));
         }
 
         public void DestroyObjects()
         {
             if (activeObjects.Any())
+            {
                 GameObjectHelper.Destroy(activeObjects);
+                activeObjects.Clear();
+            }
         }
     }
 }
